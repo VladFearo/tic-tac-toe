d0d1a4e83d956f76e563dcd6250624a58e3f4086
Vlad Shevtsov <vladsh123@gmail.com>
2025-06-21 22:13:49 +0300

    Implement game board state management and square selection functionality

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,16 +1,29 @@
+import { useState } from "react";
+
 const initialGameBoard = Array(3)
   .fill(null)
   .map(() => Array(3).fill(null));
 
 function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  const handleSelectSquare = (rowIndex, colIndex) => {
+    setGameBoard((prevBoard) => {
+      const updatedBoard = [...prevBoard.map((innerArray) => [...innerArray])];
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  };
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex} className="game-row">
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
