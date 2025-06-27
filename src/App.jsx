f2672249d21779c079910367b69ab11b4786c6e8
Vlad Shevtsov <vladsh123@gmail.com>
2025-06-27 18:36:22 +0300

    Implement game board state management and winner detection logic

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./winning-combinations";
 
+const initialGameBoard = Array(3)
+  .fill(null)
+  .map(() => Array(3).fill(null));
+
 function getActivePlayer(gameTurns) {
   let currentPlayer = "X";
   if (gameTurns.length > 0 && gameTurns[0].player === "X") {
@@ -17,6 +21,29 @@ function App() {
   const [gameTurns, setGameTurns] = useState([]);
 
   const activePlayer = getActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
+  }
+
+  let winner = null;
+  for (const combinations of WINNING_COMBINATIONS) {
+    const [firstSquareSymbol, secondSquareSymbol, thirdSquareSymbol] =
+      combinations.map((combo) => gameBoard[combo.row][combo.column]);
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+      break;
+    }
+  }
 
   const handlePlayerChange = (rowIndex, colIndex) => {
     setGameTurns((prevTurns) => {
@@ -43,7 +70,13 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        <GameBoard onSelectSquare={handlePlayerChange} turns={gameTurns} />
+        {winner && <p>You won, {winner}!</p>}
+        {!winner && gameTurns.length === 9 && (
+          <div className="draw">
+            <h2>It's a draw!</h2>
+          </div>
+        )}
+        <GameBoard onSelectSquare={handlePlayerChange} gameBoard={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,28 +1,4 @@
-const initialGameBoard = Array(3)
-  .fill(null)
-  .map(() => Array(3).fill(null));
-
-function GameBoard({ onSelectSquare, turns }) {
-  let gameBoard = initialGameBoard;
-
-  if (turns.length > 0) {
-    for (const turn of turns) {
-      const { square, player } = turn;
-      const { row, col } = square;
-      gameBoard[row][col] = player;
-    }
-  }
-  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  // const handleSelectSquare = (rowIndex, colIndex) => {
-  //   setGameBoard((prevBoard) => {
-  //     const updatedBoard = [...prevBoard.map((innerArray) => [...innerArray])];
-  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //     return updatedBoard;
-  //   });
-
-  //   onSelectSquare();
-  // };
+function GameBoard({ onSelectSquare, gameBoard }) {
   return (
     <ol id="game-board">
       {gameBoard.map((row, rowIndex) => (
